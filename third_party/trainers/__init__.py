2bd9570958af5df4cbb9716ccf78bb68cdcf0594
changho.lee <changho.lee@lgresearch.ai>
2024-06-03 13:46:24 +0900

    code update

--- third_party/trainers/__init__.py
+++ third_party/trainers/__init__.py
@@ -1,3 +1,4 @@
 from .seq2seq_trainer import Seq2SeqTrainer
 from .data_collator import TaskDataCollatorForSeq2Seq
 from .postprocessors import PostProcessor
+from .utils import ids_to_clean_text, _rougel_score, metric_rouge_english, metric_rouge_korean
